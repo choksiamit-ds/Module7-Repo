8fd3def46d1ce6d31709ad40c551d326f309ec74
choksiamit-ds <103329721+choksiamit-ds@users.noreply.github.com>
2022-06-28 15:43:05 -0700

    Update Employee_Database_Challenge.sql

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,10 +1,75 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+select * from titles;
+select * from employees;
+select * from departments;
+select * from dept_emp;
+select * from dept_manager;
+select * from employees;
+select * from retiring_titles;
+select count(*) from unique_titles;
+select count(*) from employees;
+select count (*) from dept_emp where to_date = '9999-01-01';
 
-INTO nameyourtable
-FROM _______
-WHERE _______
-ORDER BY _____, _____ DESC;
+
+-- inner join on dept_emp
+
+SELECT                 --DISTINCT ON (ee.emp_no) 
+	ee.emp_no,
+	ee.first_name,
+	ee.last_name,
+	tt.title,
+	de.from_date,
+	de.to_date
+INTO retirement_titles
+FROM employees as ee
+INNER JOIN dept_emp AS de
+	ON ( de.emp_no = ee.emp_no)
+INNER JOIN titles AS tt
+	ON (ee.emp_no = tt.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+	AND 
+	(ee.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY  ee.emp_no ASC, tt.to_date DESC;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+
+SELECT DISTINCT ON (ee.emp_no) 
+	ee.emp_no,
+	ee.first_name,
+	ee.last_name,
+	tt.title
+INTO unique_titles
+FROM employees as ee
+INNER JOIN dept_emp AS de
+	ON ( de.emp_no = ee.emp_no)
+INNER JOIN titles AS tt
+	ON (ee.emp_no = tt.emp_no)
+WHERE (de.to_date = '9999-01-01') 
+	AND (ee.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY  ee.emp_no ASC, tt.to_date DESC;
+
+
+select unique_titles.title, count(unique_titles.title) 
+  into retiring_titles
+FROM unique_titles 
+GROUP BY unique_titles.title
+ORDER BY count(unique_titles.title) desc;
+
+
+SELECT DISTINCT ON (ee.emp_no) 
+	ee.emp_no,
+	ee.first_name,
+	ee.last_name,
+	ee.birth_date,
+	de.from_date,
+	de.to_date,
+	tt.title
+INTO mentorship_eligibility
+FROM employees as ee
+INNER JOIN dept_emp AS de
+	ON ( de.emp_no = ee.emp_no)
+INNER JOIN titles AS tt
+	ON (ee.emp_no = tt.emp_no)
+WHERE (ee.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+     AND (de.to_date = '9999-01-01')
+ORDER BY  ee.emp_no ASC;
